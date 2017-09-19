38787b2837454b944b90ae4f9776b1d71f0e7bc5
banakin <banakin9@gmail.com>
2017-09-19 09:44:18 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import time, ctypes
-while 0:
+while 0 == 0:
     ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door open", None, 0, None)
     print("open")
     ctypes.windll.WINMM.mciSendStringW(u"set cdaudio door closed", None, 0, None)
